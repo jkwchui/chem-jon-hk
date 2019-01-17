8ad33f2c5eaf38449e5f5d906762bda79c73845f
Jon Chui <jon@lpcuwc.edu.hk>
2019-01-17 21:25:55 +0800

    Relocate config to LaTeX component
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/LaTeX.vue
+++ docs/.vuepress/components/LaTeX.vue
@@ -4,6 +4,43 @@
 
 <script>
 import { VueMathjax } from 'vue-mathjax'
+
+window.MathJax = {
+    extensions: ["tex2jax.js"],
+    TeX: {
+        equationNumbers: {
+            autoNumber: "AMS"
+        },
+        extensions: ["mhchem.js", "cancel.js", "color.js"],
+        Macros: {
+            gas: ["{\\color{Red}\\mathrm{_{(g)}}}"],
+            liquid: ["{\\color{SandyBrown}\\mathrm{_{(l)}}}"],
+            solid: ["{\\color{DarkSlateGrey}\\mathrm{_{(s)}}}"],
+            aq: ["{\\color{SteelBlue}\\mathrm{_{(aq)}}}"],
+            cmc: ["{\\mathrm{cm^{3}}}"],
+            dmc: ["{\\mathrm{dm^{3}}}"],
+
+        }
+    },
+    tex2jax: {
+        inlineMath: [
+            ['$', '$'],
+            ["\\(", "\\)"]
+        ],
+        displayMath: [
+            ['$$', '$$'],
+            ["\\[", "\\]"]
+        ],
+        processEscapes: true
+    },
+    mhchem: {
+        legacy: false
+    },
+    AuthorInit: function () {
+        // MathJax.Ajax.config.path["siunitx"] = "../siunitx.js";
+    }
+};
+
     export default {
         beforeMount() {
             var body = document.getElementsByTagName("body")[0], script;
