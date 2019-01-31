6c47dce85f62a5283ba51dc1d47293ee53c2b94f
Jon Chui <jon@lpcuwc.edu.hk>
2019-01-31 22:37:53 +0800

    added jsmol static page
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/Index.vue
+++ docs/.vuepress/components/Index.vue
@@ -1,21 +1,13 @@
 <template>
     <div class="content">
-        <vue-csv-import url="https://sample-videos.com/csv/Sample-Spreadsheet-10-rows.csv" :map-fields="[field1, field2, field3]"></vue-csv-import>
-        {{ field1 }}
-        {{ field2 }}
-        {{ field3 }}
     </div>
 </template>
 <script>
-import { VueCsvImport } from 'vue-csv-import'
 
     export default {
-        components: { VueCsvImport },
+        components: { },
         data () {
             return {
-                field1,
-                field2,
-                field3
             }
         }
     }
