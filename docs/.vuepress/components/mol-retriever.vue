46b3f638f1d86a2a43ece376a37443f6263cdf2c
Jon Chui <jon@lpcuwc.edu.hk>
2019-02-06 00:10:56 +0800

    attempted molecule retriever
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

---
+++ docs/.vuepress/components/mol-retriever.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <el-input
+        placeholder="Chem ID here"
+        v-model="input"
+        clearable>
+        <!-- <el-button @click="show_svg" slot="append" icon="el-icon-download"></el-button> -->
+      </el-input>
+
+      <Molecule v-bind:cid="input" />
+
+      <!-- <img :src="render"> </img> -->
+<!--       
+      {{ this.render }} -->
+
+    </div>
+</template>
+<script>
+
+    export default {
+        components: { },
+        data () {
+            return {
+              input: '314'
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- docs/chem/resources/tools/README.md
+++ docs/chem/resources/tools/README.md
@@ -1,5 +1,18 @@
 ## Typeset chemical equations
 
-Type your chemical equation in the box to have it rendered correctly.  Hint: see [mhchem manual](https://mhchem.github.io/MathJax-mhchem/) for full syntax.
+Type your chemical equation in the box to have it rendered.  Use `\solid{}`, `\aq{}`, `\gas{}`, and `\liquid{}` for state symbols, and `^` / `_` to force super/subscripts.
+
+::: tip
+
+Try pasting `Mg + HNO3 -> H2\gas{} + Mg(NO3)2\aq{}`.
+
+
+See [mhchem manual](https://mhchem.github.io/MathJax-mhchem/) for additional syntax.
+
+:::
 
 <equation-typer />
+
+<!-- ## Retrieve 3D structure
+
+<mol-retriever /> -->
