8caaac18a9276da002521830763684c5e7741871
Jon Chui <jon@lpcuwc.edu.hk>
2019-02-10 22:44:34 +0800

    added vue-pdf package
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/IA/CriteriaPie.vue
+++ docs/.vuepress/components/IA/CriteriaPie.vue
@@ -71,41 +71,7 @@ export default {
                 ]
             }
         ]
-          }
-      // polar: {
-      //   title: {
-      //     text: '极坐标双数值轴'
-      //   },
-      //   legend: {
-      //     data: ['line']
-      //   },
-      //   polar: {
-      //     center: ['50%', '54%']
-      //   },
-      //   tooltip: {
-      //     trigger: 'axis',
-      //     axisPointer: {
-      //       type: 'cross'
-      //     }
-      //   },
-      //   angleAxis: {
-      //     type: 'value',
-      //     startAngle: 0
-      //   },
-      //   radiusAxis: {
-      //     min: 0
-      //   },
-      //   series: [
-      //     {
-      //       coordinateSystem: 'polar',
-      //       name: 'line',
-      //       type: 'line',
-      //       showSymbol: false,
-      //       data: data
-      //     }
-      //   ],
-      //   animationDuration: 2000
-      // }
+      }
     }
   }
 }
