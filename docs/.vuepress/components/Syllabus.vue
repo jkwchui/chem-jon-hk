8002ffb363698edc31c843eb7231e37da192d0dc
Jon Chui <jon@lpcuwc.edu.hk>
2019-02-15 17:59:53 +0800

    added jsons, more Syllabus stub
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/Syllabus.vue
+++ docs/.vuepress/components/Syllabus.vue
@@ -1,6 +1,35 @@
 <template>
     <div>
 
+      <el-tooltip placement="top">
+        <div slot="content">
+          <div v-if="showParent">
+             <!-- info about parent -->
+             <hr />
+          </div>
+
+          <b>{{ subsubtopic }} - {{ content }}</b>
+          
+          <div v-if="showChildren">
+            <hr />
+             <!-- info about parent -->
+          </div>
+        </div>
+
+        <el-button 
+          v-if="level === 'HL'" 
+          color="#DE6328" 
+          icon="el-icon-edit">
+            {{ subsubtopic }}
+        </el-button>
+        <el-button 
+          v-else 
+          color="#009B90" 
+          icon="el-icon-edit">
+            {{ subsubtopic }}
+        </el-button>
+
+      </el-tooltip>
       <!-- http://element.eleme.io/#/en-US/component/tag -->
       <!-- http://element.eleme.io/#/en-US/component/tooltip -->
 
@@ -8,23 +37,34 @@
 </template>
 <script>
 
-// data files under /public/data/*.csv
-// probably would want to permanently convert to .json for easier access
-// Under sandbox there is working code for pulling in CSV
+import topics from '../public/data/topic.json'
+import subtopics from '../public/data/subtopic.json'
+import subsubtopics from '../public/data/subsubtopic.json'
 
 export default {
     components: { },
     data () {
         return {
-          level: 'core',
-          option: false
         }
     },
     props: {
       topic: '',
       subtopic: '',
       subsubtopic: '',
-      lang: 'en'
+      lang: 'en',
+      showParent: true,
+      showChildren: true
+    },
+    computed: {
+      content: function () {
+        return
+      },
+      level: function () {
+        return
+      },
+      option: function () {
+        return
+      } 
     }
 }
 </script>
