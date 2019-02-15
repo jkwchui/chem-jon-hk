72a85a50954ff1003849e6038adcd4eb913a3de4
Jon Chui <jon@lpcuwc.edu.hk>
2019-02-15 17:32:24 +0800

    added stub for syllabus
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- docs/.vuepress/components/Index.vue
+++ docs/.vuepress/components/Index.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="content">
+    <div>
     </div>
 </template>
 <script>
 
-    export default {
-        components: { },
-        data () {
-            return {
-            }
+export default {
+    components: { },
+    data () {
+        return {
         }
     }
+}
 </script>
 
 <style scoped>
---
+++ docs/.vuepress/components/Syllabus.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+
+      <!-- http://element.eleme.io/#/en-US/component/tag -->
+      <!-- http://element.eleme.io/#/en-US/component/tooltip -->
+
+    </div>
+</template>
+<script>
+
+// data files under /public/data/*.csv
+// probably would want to permanently convert to .json for easier access
+// Under sandbox there is working code for pulling in CSV
+
+export default {
+    components: { },
+    data () {
+        return {
+          level: 'core',
+          option: false
+        }
+    },
+    props: {
+      topic: '',
+      subtopic: '',
+      subsubtopic: '',
+      lang: 'en'
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
