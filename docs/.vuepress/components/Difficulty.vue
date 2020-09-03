a3cd167f78abab4b11ae63fc3786fabe85d294fd
Jon Chui <jon@lpcuwc.edu.hk>
2020-09-04 04:20:13 +0800

    dynamic Tasks with interactive sliders
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

---
+++ docs/.vuepress/components/Difficulty.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-rate
+    :value="lvl"
+    :colors="colors"
+    disabled>
+  </el-rate>
+</template>
+<script>
+
+export default {
+    components: { },
+    props: {
+      stars: Number
+    },
+    data () {
+      return {
+        colors: ['#6dbe45', '#f1c51b', '#d62e27']
+      }
+    },
+    computed: {
+      lvl: function() {
+        return this.stars
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
