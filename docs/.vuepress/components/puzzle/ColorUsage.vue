dafc52bacab685f67868b1d1f63b04a9fb4fc234
jkwchui <jon@lpcuwc.edu.hk>
2020-09-05 13:13:07 +0800

    Scaffold for Y1 Week 1

---
+++ docs/.vuepress/components/puzzle/ColorUsage.vue
@@ -0,0 +1,67 @@
+<template lang='pug'>
+
+.puzzle
+  task(
+    bkg='410948696' 
+    :time='0.5'
+    )
+    
+    template(v-slot:title='')
+      | Using Colors
+
+    template(v-slot:puzzle='')
+      p Solve these without a calculator.  What is #[span {{someValue}}] x #[span {{someMul}}]?
+      //- img(src="/image/IB_M20/subject-path.png")
+      el-slider(
+        v-model="someValue"
+        :step="4"
+        show-stops
+        show-input
+      )
+      el-slider(
+        v-model="someMul"
+        :step="10"
+        show-stops
+        show-input
+      )
+      ul 
+        li #[Difficulty(:stars="1")] 10 x 20
+        li #[Difficulty(:stars="2")] 22 x 30
+        li #[Difficulty(:stars="3")] 68 x 70
+      p Can you do each of these within 10 seconds?
+
+    template(v-slot:help='')
+      p Have you #[b thought] about doing this in #[a(href="https://www.google.com") multiple] bites?
+      el-collapse(v-model="activeHint" accordion)
+        el-collapse-item(title="Step 1" name="1")
+          | Try multiplying in two steps.
+        el-collapse-item(title="Step 2" name="2")
+          | 20 = 10 x 2.
+
+    template(v-slot:reflect='')
+      p #[span {{someValue}}] x #[span {{someMul}}] 
+      p #[span {{someValue}}] x 10 x #[span {{someMul / 10}}]
+      p #[span {{someValue * 10}}] x #[span {{someMul / 10}}]
+      p #[span {{someValue * someMul}}]
+      p Check that your answer has blah.
+      Vimeo(vid='449149492' name="sol")
+
+</template>
+
+<script>
+
+export default {
+  components: { },
+  data () {
+    return {
+      activeHint: '0',
+      someValue: 10,
+      someMul: 20
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ docs/chem/lessons/Y1/Week 1.md
@@ -0,0 +1,45 @@
+# Week 1
+
+## Preparations
+
+Print this week's notes
+* quarantine should have received a copy+red/green pens
+
+## Session 1: Boxes
+
+<puzzle-WhatYouWillLearn />
+<puzzle-GoogleFolders />
+<puzzle-GoogleSheetsSetupUI />
+
+### Homework
+
+Complete this (AirTable) form.
+
+## Session 2: Time
+
+<puzzle-GoogleCalendar />
+<puzzle-PomodoroMethod />
+<puzzle-UnitConversion />
+<puzzle-SIUnitsScientificNotation />
+<puzzle-GoogleSheetsFormula />
+
+### Homework
+
+Rainfall calculations
+
+## Session 3: Colors
+
+<puzzle-ColorUsage />
+<puzzle-GoogleSheetsCondFormat />
+<puzzle-TodoList />
+
+### Homework
+
+Hand in on ManageBac
+Due Sunday Sep 13
+Rainfall calculations conditional formatting
+Pomodoro drawing
+
+## Class Collage
+
+In this section I will post a selection of the homework your class submitted.  Check back next week!
