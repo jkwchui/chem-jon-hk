e16b5357b6cc292b344d948e752b0d7db67dac60
Jon Chui <jon@lpcuwc.edu.hk>
2020-09-05 13:22:47 +0800

    stub for answer checking
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

---
+++ docs/.vuepress/components/AnswerBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+
+export default {
+    components: { },
+    props: {
+      input
+    },
+    data () {
+        return {
+
+        }
+    },
+    computed: {
+      correctAnswer: function() {
+        return this.input
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
